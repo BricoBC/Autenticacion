60aa0510d2039e36aa73efb0a6c8ab7c33975a3b
bricobc <barajas.c.bruno.n@gmail.com>
2023-03-10 00:32:02 -0600

    Transición lógica de la primera vista a la segunda vista

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const btnStar = document.getElementById('btnStart');
-//const btnEnd = document.getElementById('btnEnd');
 const clock = document.getElementById('clock')
+
+const first_view = document.getElementById('first-view');
+const second_view = document.getElementById('second-view');
+const welcome = document.getElementById('welcome');
+const clock_start = document.getElementById('clock_start')
 
 const recognition = new webkitSpeechRecognition();
 
 recognition.continuos = true;
 recognition.lang = 'es-Es';
 recognition.interimResults = false;
-//recognition.maxAlternatives = 1;
 
 
 btnStar.addEventListener('click', ()=>{
@@ -18,7 +21,8 @@ recognition.onresult = (event) => {
     const texto = event.results[event.results.length - 1][0].transcript;
     for(let i = 0; i<phrases.length; i++){
         if (texto == phrases[i])
-            alert('Bienvenido sea ' + users[i])
+            showSesion(users[i], time() )
+
     }
     console.log(texto)
 }
@@ -32,13 +36,23 @@ users = [
     'Administrador', 'Ayudante', 'Usuario normal'
 ]
 
-function mueveReloj(){
+function showSesion(person, hora){
+    second_view.classList.remove('inactive');
+    first_view.classList.add('inactive');
+    welcome.innerText = 'Bienvenido sea ' + person
+    clock_start.innerText = 'Inicio sesión a las ' + hora
+}
+
+function time(){
     momentoActual = new Date()
     hora = momentoActual.getHours()
     minuto = momentoActual.getMinutes()
     segundo = momentoActual.getSeconds()
+    return hora + ':' + minuto + ':' + segundo
+}
 
-    clock.innerText = 'La hora es ' + hora + ':' + minuto + ':' + segundo
+function mueveReloj(){
+    clock.innerText = 'La hora es ' + time()
 
     setTimeout("mueveReloj()",1000)
 }
