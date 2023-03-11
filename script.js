1ce4674a31439af436c60124a98235964cf406ff
bricobc <barajas.c.bruno.n@gmail.com>
2023-03-10 23:23:52 -0600

    boton que nos regresa al inicio

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const btnStar = document.getElementById('btnStart');
 const clock = document.getElementById('clock')
-
+const login_view = document.getElementById('login-view')
 const first_view = document.getElementById('first-view');
 const second_view = document.getElementById('second-view');
 const welcome = document.getElementById('welcome');
 const clock_start = document.getElementById('clock_start')
+const btnCloseSesion = document.getElementById('btnCloseSesion')
 
 const recognition = new webkitSpeechRecognition();
 
+const arrAllViews = [first_view, second_view, login_view]
+
+function openView(view){
+    for(let i = 0 ; i < arrAllViews.length; i++){
+        if(view == arrAllViews[i])
+        {
+            arrAllViews[i].classList.remove('inactive');
+        }else{
+            arrAllViews[i].classList.add('inactive');
+        }
+        console.log(arrAllViews[i])
+    }
+}
 recognition.continuos = true;
 recognition.lang = 'es-Es';
 recognition.interimResults = false;
@@ -16,6 +30,10 @@ recognition.interimResults = false;
 btnStar.addEventListener('click', ()=>{
     recognition.start();
 });
+
+btnCloseSesion.addEventListener('click', ()=>{
+    openView(login_view);
+})
 
 recognition.onresult = (event) => {
     const texto = event.results[event.results.length - 1][0].transcript;
@@ -53,6 +71,5 @@ function time(){
 
 function mueveReloj(){
     clock.innerText = 'La hora es ' + time()
-
     setTimeout("mueveReloj()",1000)
 }
